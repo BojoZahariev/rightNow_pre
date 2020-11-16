d89a2867fe9be39515f48573acd1fa0315d39cac
Bojo <bozhidar_zahariev@hotmail.com>
2020-11-16 09:56:32 +0000

    remove

--- script.js
+++ script.js
@@ -20,17 +20,14 @@ function App() {
   React.useEffect(() => {
     setTimeout(() => setSeconds(seconds + 1), 1000);
   });
-  //let childrenCount = Math.floor(seconds / 5);
-  //let cancerCount = Math.floor(seconds / 3);
+
   return (
     <div className='App'>
       <TimePassed secs={seconds} />
       <div className='perksContainer'>
-        {/*<Perk title={'Children under 15 deaths'} secs={seconds % 5 === 0 ? seconds / 5 : childrenCount} link={'www.something.com'} />*/}
         <Perk title={'Trees cut'} secs={seconds * 475} link={'www.something.com'} />
         <Perk title={'Plastic pieces in the ocean'} secs={seconds * 93} link={'www.something.com'} />
         <Perk title={'Kilograms of CO2 emitted in the air'} secs={seconds * 1165732} link={'www.something.com'} />
-        {/*<Perk title={'Cancer related deaths'} secs={seconds % 3 === 0 ? seconds / 3 : cancerCount} link={'www.something.com'} />*/}
         <Perk title={'Kilograms of Ice melting in Antarctica'} secs={seconds * 8000000} link={'www.something.com'} />
         <Perk title={'Kilograms of methane emitted in the air'} secs={seconds * 18000} link={'www.something.com'} />
       </div>
